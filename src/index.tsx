35a0df951f8b274752c676411b5578a6a87082a2
Zack Yew <yewb652@gmail.com>
2024-10-27 22:18:29 -0400

    installed webpack

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = document.createElement('div');
+root.className = 'container';
+document.body.appendChild(root);
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
