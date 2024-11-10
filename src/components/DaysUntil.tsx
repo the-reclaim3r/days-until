a0fcfd35cc00391a939ba3069347dda37d659f9d
Zack Yew <yewb652@gmail.com>
2024-11-10 00:41:05 -0500

    reload on clear

--- src/components/DaysUntil.tsx
+++ src/components/DaysUntil.tsx
@@ -6,29 +6,37 @@ const DaysUntil = () => {
 	const [eventName, setEventName] = useState<string | null>('');
 	const [targetDate, setTargetDate] = useState<string | null>();
 
-  let hours = dayjs(targetDate).diff(dayjs(), 'hours');
-  const days = Math.floor(hours / 24);
-  hours = hours - (days * 24); // TODO maybe: allow user to choose time for events
+	let hours = dayjs(targetDate).diff(dayjs(), 'hours');
+	const days = Math.floor(hours / 24);
+	hours = hours - days * 24; // TODO maybe: allow user to choose time for events
 
 	useEffect(() => {
 		setEventName(window.localStorage.getItem('event-name'));
 		setTargetDate(window.localStorage.getItem('target-date'));
 	}, []);
 
-  return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      gap={2}
-    >
-      <Typography variant='h1' fontWeight={400}>
-        {days} days until {eventName}
-      </Typography>
-      <Button variant="outlined" onClick={() => window.localStorage.clear()}>Clear</Button>
-    </Box>
-  );
+	return (
+		<Box
+			display='flex'
+			justifyContent='center'
+			alignItems='center'
+			flexDirection='column'
+			gap={2}
+		>
+			<Typography variant='h1' fontWeight={400}>
+				{days} days until {eventName}
+			</Typography>
+			<Button
+				variant='outlined'
+				onClick={() => {
+					window.localStorage.clear();
+					window.dispatchEvent(new Event('days-until'));
+				}}
+			>
+				Clear
+			</Button>
+		</Box>
+	);
 };
 
 export default DaysUntil;
