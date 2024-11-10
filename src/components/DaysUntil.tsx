5d586c1413b31bbb4472002b722a5a16eb863fb7
Zack Yew <yewb652@gmail.com>
2024-11-10 00:22:37 -0500

    linting

--- src/components/DaysUntil.tsx
+++ src/components/DaysUntil.tsx
@@ -1,33 +1,34 @@
-import { Box, Button, Typography } from "@mui/material";
-import dayjs, { Dayjs } from "dayjs";
-import React, { useEffect, useState } from "react";
+import { Box, Button, Typography } from '@mui/material';
+import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
 
 const DaysUntil = () => {
-  const [eventName, setEventName] = useState<string | null>("");
-  const [targetDate, setTargetDate] = useState<string | null>();
+	const [eventName, setEventName] = useState<string | null>('');
+	const [targetDate, setTargetDate] = useState<string | null>();
 
-  const dayDifference = dayjs(targetDate).diff(dayjs(), "day");
+	const dayDifference = dayjs(targetDate).diff(dayjs(), 'day');
 
-  useEffect(() => {
-    // GET STUFF FROM LOCALSTORAGE HERE REID
-    setEventName(window.localStorage.getItem("event-name"));
-    setTargetDate(window.localStorage.getItem("target-date"));
-  });
+	useEffect(() => {
+		setEventName(window.localStorage.getItem('event-name'));
+		setTargetDate(window.localStorage.getItem('target-date'));
+	}, []);
 
-  return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      gap={2}
-    >
-      <Typography variant="h3">
-        {dayDifference} Days until {eventName}
-      </Typography>
-      <Button variant="outlined" onClick={() => window.localStorage.clear()}>Clear</Button>
-    </Box>
-  );
+	return (
+		<Box
+			display='flex'
+			justifyContent='center'
+			alignItems='center'
+			flexDirection='column'
+			gap={2}
+		>
+			<Typography variant='h3'>
+				{dayDifference} Days until {eventName}
+			</Typography>
+			<Button variant='outlined' onClick={() => window.localStorage.clear()}>
+				Clear
+			</Button>
+		</Box>
+	);
 };
 
 export default DaysUntil;
