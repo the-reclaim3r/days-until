ff29c0b97084469556dc5404f25e952aa9a54709
Reid McInroy <reid.mcinroy@gmail.com>
2024-11-12 16:10:53 -0500

    Update DaysUntil.tsx
    Changed logic for displaying the countdown string. Restructured code to allow for refreshing the component

--- src/components/DaysUntil.tsx
+++ src/components/DaysUntil.tsx
@@ -1,21 +1,57 @@
 import { Box, Button, Typography } from '@mui/material';
 import dayjs from 'dayjs';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const DaysUntil = () => {
 	const [eventName, setEventName] = useState<string | null>('');
 	const [targetDate, setTargetDate] = useState<string | null>();
 
-	let hours = dayjs(targetDate).diff(dayjs(), 'hours');
-	const days = Math.floor(hours / 24);
-	hours = hours - days * 24;
-	let minutes = dayjs(targetDate).diff(dayjs(), 'minutes');
-	minutes = Math.floor(minutes - hours * 60); // TODO refresh display of minutes every minute
+	const [timeLeft, setTimeLeft] = useState('');
 
 	useEffect(() => {
 		setEventName(window.localStorage.getItem('event-name'));
 		setTargetDate(window.localStorage.getItem('target-date'));
 	}, []);
+
+	const calculateTimeLeft = useCallback(() => {
+		let hours = dayjs(targetDate).diff(dayjs(), 'hours');
+		const days = Math.floor(hours / 24);
+		hours = hours - days * 24;
+		let minutes = dayjs(targetDate).diff(dayjs(), 'minutes');
+		minutes = Math.floor(minutes - hours * 60); // TODO refresh output string every minute
+
+		// Event is in the past
+		if (dayjs(targetDate).diff(dayjs()) < 0) {
+			return 'This event has passed';
+		}
+
+		let timeString = 'in ';
+		if (days > 0) {
+			timeString += `${days} day${days === 1 ? '' : 's'} and `;
+		}
+
+		if (hours > 0) {
+			timeString += `${hours} hour${hours === 1 ? '' : 's'}`;
+			if (days === 0 && minutes > 0) {
+				timeString += ' and ';
+			}
+		}
+
+		if (days === 0 && minutes > 0) {
+			timeString += `${minutes} minute${minutes === 1 ? '' : 's'}`;
+		}
+
+		return timeString;
+	}, [targetDate]);
+
+	useEffect(() => {
+		setTimeLeft(calculateTimeLeft());
+		const interval = setTimeout(() => {
+			setTimeLeft(calculateTimeLeft());
+		}, 10000);
+
+		return () => clearTimeout(interval);
+	}, [calculateTimeLeft]);
 
 	return (
 		<Box
@@ -29,8 +65,7 @@ const DaysUntil = () => {
 				{eventName}
 			</Typography>
 			<Typography variant='h2' fontWeight={400}>
-				in {days ? days + ' days and ' : ''} {hours} hours{' '}
-				{days ? '' : 'and ' + minutes + ' minutes'}
+				{timeLeft}
 			</Typography>
 			<Button
 				variant='outlined'
